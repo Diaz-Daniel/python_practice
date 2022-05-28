20785af27a045f658200014207f0c2adb7b3fabf
DDiaz <daniel.diaz.0515@gmail.com>
2022-05-28 12:48:48 -0700

    data types review

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
-print("hello world")
-Print("This is my first python program")
+#print("hello world")
+#print("This is my first python program")
+
+# Strings
+#name = 'daniel'
+# print(name)
+
+# checking type
+# print(type(name))
+
+#first_name = 'abi'
+#last_name = 'diaz'
+#full_name = first_name + last_name
+#print('Hello' + ' ' + full_name)
+
+# working with numbers
+#age = 32
+#age += 1
+# print(age)
+# print(type(age))
+
+# converting int to string when printing result
+# this is called a stringcast
+#print("this year you will be: " + str(age))
+
+# float datatype (includes numbers with decimal
+#height = 250.5
+# print(height)
+# print(type(height))
+#print("an object's height is: " + str(height)+"cm")
+
+# boolean true or false
+human = False
+# print(human)
+# print(type(human))
+print('are you a human: ' + str(human))
