9256b82c7c950bb875f34b113856cd635d003226
DDiaz <daniel.diaz.0515@gmail.com>
2022-06-28 20:25:20 -0700

    Accepting User Input

--- main.py
+++ main.py
@@ -53,7 +53,7 @@
 # print(squidward)
 
 # USEFUL METHODS FOR STRINGS
-name = "daniel diaz"
+#name = "daniel diaz"
 # print(len(name))
 # print(name.find("l"))
 # print(name.capitalize())
@@ -63,4 +63,26 @@ name = "daniel diaz"
 # print(name.isalpha())
 # print(name.count("a"))
 #print(name.replace("a", "e"))
-print(name*3)
+# print(name*3)
+
+# TYPE CASTING = convert the data type of a value to another data type
+# this is a helpful method because you cannot print a string with an integer using concatonation, so
+# converting this is the easiest option
+# x = 1  # int
+# y = 2.0  # float
+# z = "3"  # str
+
+#z = int(z)
+# y = int(y)  # this is a long term change to y
+# print(x)
+# print(int(y)) # this is a short term reassignment to change y from float to int
+# print(z)
+# print(y)
+# print(z*3) # this is possible because on line 74 we changed z to an int
+# print("x is an int but is now a short term string " + str(x))
+
+
+# ACCEPTING USER INPUT
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+print("Hello "+name)
